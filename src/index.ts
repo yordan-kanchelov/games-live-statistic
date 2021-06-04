d287991ab57fa99cdc1bc8613f8890d6d1a5ba9d
jkanchelov <yordan.kanchelov@gmail.com>
2021-06-04 23:44:07 +0300

    feat: create initial express server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-import { GamesLiveStatistic } from "./games-live-statistic";
+import express from "express";
+const app = express();
+const port = process.env.PORT || 3000;
 
-export { GamesLiveStatistic };
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// import { GamesLiveStatistic } from "./games-live-statistic";
+
+// export { GamesLiveStatistic };
